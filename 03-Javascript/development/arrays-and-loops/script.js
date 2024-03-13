baefc3557264570e149773c61e58d6fca2f7b71e
Bethany Burrows <jedcburrows@gmail.com>
2024-03-13 10:11:50 +0000

    Created script.js to help learn arrays and loops

---
+++ 03-Javascript/development/arrays-and-loops/script.js
@@ -0,0 +1,24 @@
+//Coaches array
+const coaches = ["Andy", "Sam", "Bex"]
+console.log (coaches[0])
+
+//Pets array
+const pets = ["Nibbles", "Waffles", "Alfie", "Brownie"]
+console.log (pets[1])
+
+//Numbers array
+const numbers = [7, 15, 1, 2, 19, 7, 25, 6, 13, 6, 7, 15, 14, 0]
+for (let index = 0; index < numbers.length; index++)
+{
+    console.log (numbers[index])
+}
+
+//True/False array
+const truth = [0, 1]
+console.log (1)
+
+//Sample loop
+for (let index = 0; index < 5; index++)
+{
+    console.log ("The index on this loop was " + index)
+}
